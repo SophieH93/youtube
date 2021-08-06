e7fc534d0fd6990c7a8b5b0d185d9cfd0ab0f0ab
Sophie <sophiehickey123@gmail.com>
2021-08-06 18:21:50 +0100

    added dearchInput functionality

--- js/app.js
+++ js/app.js
@@ -53,3 +53,14 @@ const makeVideoCard = (data) => {
     </div>     
     `;
 };
+
+// search bar for
+const searchInput = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-btn");
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener("click", () => {
+  if (searchInput.value.length) {
+    location.href = searchLink + searchInput.value;
+  }
+});
